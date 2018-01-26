45b026995a392be5462ab8b9c2234aec278e32b2
Leessang <992275294@qq.com>
2018-01-26 14:42:54 +0800

    Initial Commit

---
+++ KNScrollVideoPaly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNScrollVideoPaly
+//
+//  Created by 刘凡 on 2018/1/26.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNScrollVideoPaly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNScrollVideoPaly
+//
+//  Created by 刘凡 on 2018/1/26.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
