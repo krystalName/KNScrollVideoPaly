b4de85361f1bed5e19cdadd0b762d5e2303a32bd
Leessang <992275294@qq.com>
2018-01-31 11:49:36 +0800

    完成配置编写

--- KNScrollVideoPaly/Player/KNVideoSlider.m
+++ KNScrollVideoPaly/Player/KNVideoSlider.m
@@ -11,7 +11,7 @@
 
 @interface KNVideoSlider()
 {
-    CALayer *_lienLayer;
+    CALayer *_lineLayer;
     KNLayerDelegate *_delegate;
 }
 
@@ -40,10 +40,10 @@
         _delegate.sliderColor = self.sliderColor;
         _delegate.lineWidth = self.lineWidth;
         
-        _lienLayer = [CALayer layer];
-        _lienLayer.delegate = _delegate;
-        [self.layer addSublayer:_lienLayer];
-        [_lienLayer setNeedsDisplay];
+        _lineLayer = [CALayer layer];
+        _lineLayer.delegate = _delegate;
+        [self.layer addSublayer:_lineLayer];
+        [_lineLayer setNeedsDisplay];
         
         [self addObserver:self forKeyPath:@"value" options:NSKeyValueObservingOptionNew context:nil];
         [self addObserver:self forKeyPath:@"middleValue" options:NSKeyValueObservingOptionNew context:nil];
@@ -58,12 +58,24 @@
     [super layoutSubviews];
     _delegate.centerY = self.frame.size.height/2.0f;
     _delegate.lineLength = self.frame.size.width;
-    _lienLayer.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-    [_lienLayer setNeedsDisplay];
+    _lineLayer.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
+    [_lineLayer setNeedsDisplay];
     
 }
 
 #pragma mark - KeyValue实现
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
+    if([keyPath isEqualToString:@"value"]){
+        [_lineLayer setNeedsDisplay];
+        if (self.valueChangeBlock) {
+            self.valueChangeBlock(self);
+        }
+    }
+    if ([keyPath isEqualToString:@"middleValue"]) {
+        [_lineLayer setNeedsDisplay];
+    }
+}
 
 
 
@@ -99,6 +111,102 @@
     }
 }
 
+#pragma mark - setter 和 getter
 
+@synthesize sliderColor = _sliderColor;
+@synthesize lineWidth = _lineWidth;
+@synthesize minColor = _minColor;
+@synthesize middleColor = _middleColor;
+@synthesize maxColor = _maxColor;
+@synthesize sliderDiameter = _sliderDiameter;
+
+- (void)setSliderColor:(UIColor *)sliderColor {
+    _sliderColor = sliderColor;
+    _delegate.sliderColor = _sliderColor;
+}
+
+- (UIColor *)sliderColor {
+    if (!_sliderColor) {
+        return [UIColor whiteColor];
+    }
+    return _sliderColor;
+}
+
+- (void)setSliderDiameter:(CGFloat)sliderDiameter {
+    _sliderDiameter = sliderDiameter;
+    _delegate.sliderDiameter = sliderDiameter;
+}
+
+- (CGFloat)sliderDiameter {
+    if (!_sliderDiameter) {
+        return 10.0f;
+    }
+    return _sliderDiameter;
+}
+
+- (void)setMinColor:(UIColor *)minColor {
+    _minColor = minColor;
+    _delegate.minColor = minColor;
+}
+
+- (UIColor *)minColor {
+    if (!_minColor) {
+        return [UIColor greenColor];
+    }
+    return _minColor;
+}
+
+- (void)setMaxColor:(UIColor *)maxColor {
+    _maxColor = maxColor;
+    _delegate.maxColor = maxColor;
+}
+
+- (UIColor *)maxColor {
+    if (!_maxColor) {
+        return [UIColor darkGrayColor];
+    }
+    return _maxColor;
+}
+
+- (void)setMiddleColor:(UIColor *)middleColor {
+    _middleColor = middleColor;
+    _delegate.middleColor = middleColor;
+}
+
+- (UIColor *)middleColor {
+    if (!_middleColor) {
+        return  [UIColor lightGrayColor];
+        //        return  [UIColor redColor];
+    }
+    return _middleColor;
+}
+
+- (CGFloat)lineWidth {
+    if (!_lineWidth) {
+        return 1.0f;
+    }
+    return _lineWidth;
+}
+
+- (void)setLineWidth:(CGFloat)lineWidth {
+    _lineWidth = lineWidth;
+    _delegate.lineWidth = lineWidth;
+}
+
+-(void)setMiddleValue:(CGFloat)middleValue {
+    _middleValue = middleValue;
+    _delegate.middleValue = middleValue;
+}
+
+- (void)setValue:(CGFloat)value {
+    _value = value;
+    panDistance = value * (self.frame.size.width - self.sliderDiameter);
+}
+
+///删除监听
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"value"];
+    [self removeObserver:self forKeyPath:@"middleValue"];
+}
 
 @end
