fd2b7595da8fb076de22086198a4deeb13c4ad04
Leessang <992275294@qq.com>
2018-01-31 15:57:06 +0800

    完善项目中

--- KNScrollVideoPaly/Player/KNVideoPlayerView.h
+++ KNScrollVideoPaly/Player/KNVideoPlayerView.h
@@ -7,7 +7,39 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "KNVideoSlider.h"
+#import <AVFoundation/AVFoundation.h>
+
+
+@class KNVideoPlayerView;
+
+
+typedef void (^VideoCompletedPlayingBlock) (KNVideoPlayerView *);
+
 
 @interface KNVideoPlayerView : UIView
+
+@property (nonatomic, copy) VideoCompletedPlayingBlock completedPlayingBlock;
+@property (nonatomic, strong) KNVideoSlider *slider;
+@property (nonatomic,strong) AVPlayer *player;
+
+
+///视频路径
+@property(nonatomic, strong) NSString *videoUrl;
+
+///视频暂停
+-(void)playPause;
+
+///销毁播放器
+-(void)destroyPlayer;
+
+///在cell上播放必须绑定TableView、当前播放cell的IndexPath
+- (void)playerBindTableView:(UITableView *)bindTableView currentIndexPath:(NSIndexPath *)currentIndexPath;
+
+///在scrollview的scrollViewDidScroll代理中调用
+- (void)playerScrollIsSupportSmallWindowPlay:(BOOL)support;
+
+//进度条和播放暂停按钮
+- (void)setStatusBarHidden:(BOOL)hidden;
 
 @end
