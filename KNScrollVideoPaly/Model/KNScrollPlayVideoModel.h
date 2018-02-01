18fbfdd45155a55ba65a94952cd8e01250b72136
Leessang <992275294@qq.com>
2018-02-01 17:13:45 +0800

    创建一个model 类

---
+++ KNScrollVideoPaly/Model/KNScrollPlayVideoModel.h
@@ -0,0 +1,16 @@
+//
+//  KNScrollPlayVideoModel.h
+//  KNScrollVideoPaly
+//
+//  Created by 刘凡 on 2018/2/1.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KNScrollPlayVideoModel : NSObject
+
+@property (nonatomic, assign) BOOL isShouldToPlay;
+@property (nonatomic, assign) BOOL isLight;
+
+@end
---
+++ KNScrollVideoPaly/Model/KNScrollPlayVideoModel.m
@@ -0,0 +1,13 @@
+//
+//  KNScrollPlayVideoModel.m
+//  KNScrollVideoPaly
+//
+//  Created by 刘凡 on 2018/2/1.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import "KNScrollPlayVideoModel.h"
+
+@implementation KNScrollPlayVideoModel
+
+@end
