c6a2b43ccfa4231063f958528c9eb8320234ee20
Leessang <992275294@qq.com>
2018-02-06 15:05:20 +0800

    添加代码

--- KNScrollVideoPaly/View/KNScrollPlayerVideoView.m
+++ KNScrollVideoPaly/View/KNScrollPlayerVideoView.m
@@ -7,15 +7,33 @@
 //
 
 #import "KNScrollPlayerVideoView.h"
+#import "KNScrollPlayerVideoCell.h"
+#import "KNScrollPlayVideoModel.h"
+#import "KNVideoPlayerView.h"
+#import <AFNetworking.h>
+
+#define KNScreenWidth [UIScreen mainScreen].bounds.size.width
+#define KNScreenHeight [UIScreen mainScreen].bounds.size.height
+
+
+///接口
+#define videoListUrl @"http://c.3g.163.com/nc/video/list/VAP4BFR16/y/0-10.html"
+#define cellHeigh 320
+
+
+@interface KNScrollPlayerVideoView()<KNScrollPlayerDelegate>
+{
+    BOOL rate;
+    KNVideoPlayerView *_player;
+}
+
+@property(nonatomic, strong) UITableView *tableView;
+@property(nonatomic, strong) NSMutableArray *dataArray;
+
+@end
 
 @implementation KNScrollPlayerVideoView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+
 
 @end
